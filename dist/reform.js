74e9e056a28037c626d7377848bf4cab0a3f176b
Andy Tran <andy.tran11996@gmail.com>
2019-03-12 16:38:56 +0100

    refactored && added standalone js

--- dist/reform.js
+++ dist/reform.js
@@ -1,3 +1,24 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Reformer = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+/*
+ * moleculer-db
+ * Copyright (c) 2019 Andy Tran (https://github.com/andycillin/json-reform)
+ * MIT Licensed
+ */
+
+"use strict";
+
+module.exports = require("./src/mapper");
+},{"./src/mapper":3}],2:[function(require,module,exports){
+'use strict';
+
+function isFunction(obj) {
+    return !!(obj && obj.constructor && obj.call && obj.apply);
+}
+
+module.exports = {
+    isFunction
+};
+},{}],3:[function(require,module,exports){
 "use strict";
 const {isFunction} = require('./helper');
 
@@ -134,3 +155,5 @@ Reformer.prototype.transform = function (obj) {
 
 
 module.exports = Reformer;
+},{"./helper":2}]},{},[1])(1)
+});
