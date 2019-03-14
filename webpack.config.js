c32a62af8bd20afc054789f22c21cb2442078754
Andy Tran <andy.tran11996@gmail.com>
2019-03-14 12:33:41 +0100

    Added build script

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "reform.js",
+        library: 'Reformer',
+        libraryTarget: 'umd',
+    }
+};
